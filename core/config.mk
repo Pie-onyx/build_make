66b0159d6669096c8f50114dafeeb193173fc76e
Kshitij Gupta <kshitijgm@gmail.com>
2019-08-11 18:10:32 +0530

    config: Include custom boardconfig a bit later
    - We're trying to export MAKE_PREBUILT to soong
    - Let's include it right before soong_config.mk is included
    
    Signed-off-by: Kshitij Gupta <kshitijgm@gmail.com>

--- core/config.mk
+++ core/config.mk
@@ -230,8 +230,6 @@ include $(BUILD_SYSTEM)/envsetup.mk
 # See envsetup.mk for a description of SCAN_EXCLUDE_DIRS
 FIND_LEAVES_EXCLUDES := $(addprefix --prune=, $(SCAN_EXCLUDE_DIRS) .repo .git)
 
-include vendor/lemon/config/BoardConfigLemon.mk
-
 # The build system exposes several variables for where to find the kernel
 # headers:
 #   TARGET_DEVICE_KERNEL_HEADERS is automatically created for the current
@@ -1171,6 +1169,8 @@ dont_bother_goals := out \
     recoveryimage-nodeps \
     vbmetaimage-nodeps \
     product-graph dump-products
+
+include vendor/lemon/config/BoardConfigLemon.mk
 
 ifeq ($(CALLED_FROM_SETUP),true)
 include $(BUILD_SYSTEM)/ninja_config.mk
